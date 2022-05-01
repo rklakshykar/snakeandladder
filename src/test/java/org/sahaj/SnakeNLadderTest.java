5bffd67cc023f25791c5a5772f036f715ce9aeef
rohit <rohit.lakshykar@eko.co.in>
2022-05-01 20:09:58 +0530

    test cases

--- src/test/java/org/sahaj/SnakeNLadderTest.java
+++ src/test/java/org/sahaj/SnakeNLadderTest.java
@@ -1,5 +1,7 @@
 package org.sahaj;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,11 +12,16 @@ import org.sahaj.helper.Constants;
 import org.sahaj.player.Player;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class SnakeNLadderTest {
+    private static final Logger logger = LogManager.getLogger(SnakeNLadderTest.class);
 
     App app;
 
@@ -27,6 +34,7 @@ class SnakeNLadderTest {
     @DisplayName("SnakeNLadderSuccessTest")
     void snakeNLadderSuccessTest() throws IOException, ParseException {
         JSONObject jsonObject = app.getJsonObject("input.json");
+        //this test runs only 1 simulation
         int[][] ladders = app.extractArrayData(jsonObject, "ladders", "top", "bottom");
         int[][] snakes = app.extractArrayData(jsonObject, "snakes", "head", "tail");
         SnakeNLadder snakeNLadder = new SnakeNLadder(snakes, ladders).withPlayer(new Player(Constants.PLAYER1.value));
@@ -34,6 +42,29 @@ class SnakeNLadderTest {
             snakeNLadder.play(0);
         }
         assertEquals(true, snakeNLadder.isGameOver());
+    }
+
+    @Test
+    @DisplayName("SnakeNLadderSuccessTest")
+    void snakeNLadderNSimulationTest() throws IOException, ParseException {
+        JSONObject jsonObject = app.getJsonObject("input.json");
+        int totalSimulation = Integer.parseInt(jsonObject.get("simulations").toString());
+        int[][] ladders = app.extractArrayData(jsonObject, "ladders", "top", "bottom");
+        int[][] snakes = app.extractArrayData(jsonObject, "snakes", "head", "tail");
+        List<Player> simulations = new ArrayList<>();
+        SnakeNLadder snakeNLadder = new SnakeNLadder(snakes, ladders);
+        while (totalSimulation > 0) {
+            snakeNLadder = snakeNLadder.withPlayer(new Player(Constants.PLAYER1.value));
+            while (!snakeNLadder.isGameOver()) {
+                snakeNLadder.play(0);
+            }
+            simulations.add(snakeNLadder.player);
+            totalSimulation--;
+        }
+        simulations.forEach(player -> logger.info("{}\n", player));
+        GameStats gameStats = new GameStats(simulations);
+        gameStats.printStats();
+        assertTrue(true);
     }
 
 
